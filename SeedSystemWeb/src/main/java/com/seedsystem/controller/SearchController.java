7e171e2c19dca3abf7bd2743e6e06e4ceae50a13
Saurabh Prakash <sp.infosys@gmail.com>
2019-10-12 18:42:11 -0500

    searchdealer

--- SeedSystemWeb/src/main/java/com/seedsystem/controller/SearchController.java
+++ SeedSystemWeb/src/main/java/com/seedsystem/controller/SearchController.java
@@ -30,7 +30,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
-@RequestMapping(value = "/search")
+@RequestMapping(value = "/searchDealer")
 public class SearchController {
 
 	
